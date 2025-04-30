cd92a706ad22c2b75588285a8d0fc9aebe054606
dimitrios-apostolakis <dsapostolakis@gmail.com>
2025-04-30 09:10:24 +0300

    initializing test instances

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,17 +1,21 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 class GreetingTest {
 
     private Greeting greeting;
 
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("****Before all... called once");
+    }
+
     @BeforeEach
     void setUp() {
         System.out.println("Before each...");
 
-         greeting = new Greeting();
+        greeting = new Greeting();
     }
 
     @Test
@@ -22,5 +26,20 @@ class GreetingTest {
     @Test
     void helloWorld1() {
         System.out.println(greeting.helloWorld("Jim"));
+    }
+
+    @Test
+    void helloWorld2() {
+        System.out.println(greeting.helloWorld("Sam"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("After each...");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("****After all... called once");
     }
 }
